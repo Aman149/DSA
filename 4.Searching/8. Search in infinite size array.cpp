6b981b8c0fdf99472a9140ebc8f6834b7e8721ff
Aman149 <aman001014@gmail.com>
2022-07-28 00:01:11 +0530

    Search in infinite array O(log p)

--- 4.Searching/8. Search in infinite size array.cpp
+++ 4.Searching/8. Search in infinite size array.cpp
@@ -4,26 +4,25 @@ using namespace std;
 //Given an infinite sized array
 //we need to write an efficient solution to search an element
 
-int binarySearch(int x) {
-    
-    int low = 0, high = x, ans = -1;
-
+//Using unbounded binary search
+int binarySearch(vector<int> arr, int target) {
+    int low = 0, high = 1;
     while(low <= high) {
         int mid = (low+high)/2;
 
-        if(mid*mid == x)
+        if(arr[mid] == target)
             return mid;
-        else if(mid*mid > x)
-            high = mid - 1;
+        else if(arr[mid] > target)
+            high = mid-1;
         else {
-            ans = mid;
             low = mid + 1;
+            high = high * 2;
         }
     }
-    return ans;
+    return -1;
 }
 
 int main() {
-    
-    cout<<binarySearch(92681);
+    vector<int> arr = {6, 22, 26, 43, 58, 59, 65, 77, 81, 110, 113, 150, 180, 200, 201, 220, 260, 300, 500, 670, 800};
+    cout<<binarySearch(arr, 201);
 }
