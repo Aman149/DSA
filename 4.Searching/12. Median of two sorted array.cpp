6cab1b47aa1140cbc1a1eb5d0b35e9aa05fbdc23
Aman149 <aman001014@gmail.com>
2022-07-30 21:49:47 +0530

    Median of two sorted array O(log n)

---
+++ 4.Searching/12. Median of two sorted array.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//Find the Median of two sored arrays in O(log n)
+
+int findMedian(vector<int> arr1, vector<int> arr2) {
+    //not yet done
+}
+
+int main() {
+    vector<int> arr1 = {1, 4, 6, 7, 8, 10};
+    vector<int> arr2 = {2, 3, 9, 12, 24, 25, 27}
+    cout<<findMedian(arr1, arr2);
+}
