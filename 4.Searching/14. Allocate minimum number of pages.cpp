c6344a97ad2db92da9a4bfcac1ad36ba6d53dac1
Aman149 <aman001014@gmail.com>
2022-08-01 21:41:49 +0530

    TO DO

---
+++ 4.Searching/14. Allocate minimum number of pages.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//Allocate Minimum number of Pages
+//Famous interview Question
+
+
+int findBooksToRead(vector<int> arr, int k) {
+
+}
+
+int main() {
+    vector<int> arr = {10, 20, 30, 40};
+    cout<<findBooksToRead(arr, 2);
+}
