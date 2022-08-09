f8b65bcb230bb729a6bd8ec9786297d182497ae5
Aman149 <aman001014@gmail.com>
2022-08-09 16:24:51 +0530

    Lombuto Partition Time: O(n) , Sapce: O(1)

---
+++ 5.Sorting/13. Lombuto Partition.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int lombutoPartition(vector<int> arr, int low, int high, int p) {
+
+}
+
+int main() {
+    vector<int> arr {1, 10, 7, 5, 3, 2, 12};
+    cout<<lombutoPartition(arr, 0, arr.size(), 4);
+}
