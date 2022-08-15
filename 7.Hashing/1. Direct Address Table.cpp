38021ff6242049dfc14543a7239ea8682c05918e
Aman149 <aman001014@gmail.com>
2022-08-15 23:38:28 +0530

    Direct Addressing Time O(1), Space O(1)

---
+++ 7.Hashing/1. Direct Address Table.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int arr[1000] = {0};
+
+bool directAddressing(int x) {
+
+}
+
+int main() {
+    cout<<directAddressing(10);
+}
+#include<bits/stdc++.h>
+using namespace std;
+int arr[1000] = {0};
+
+void insert(int x) {
+    arr[x]++;
+}
+
+int search(int x) {
+    
+    if(arr[x] > 0)
+        return x;
+    else
+        return -1;
+}
+
+int main() {
+    
+    insert(10);
+    insert(20);
+    insert(119);
+    
+    cout<<search(119);
+}
