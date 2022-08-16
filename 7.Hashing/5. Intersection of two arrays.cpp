bf9d2d0296ff363cfa0729cbee9077bf610804de
Aman149 <aman001014@gmail.com>
2022-08-17 00:33:34 +0530

    Space O(n), Time O(n)

---
+++ 7.Hashing/5. Intersection of two arrays.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void intersection(int arr1[], int arr2[], int n, int m) {
+    unordered_set<int> hash;
+
+    for(int i=0; i<n; i++)
+        hash.insert(arr1[i]);
+
+    for(int i=0; i<m; i++)
+        hash.insert(arr2[i]);
+    
+    for(auto x : hash)
+        cout<<x<<" ";
+}
+
+int main() {
+    int arr1[] = {1, 1, 1, 4, 4, 5, 6, 7};
+    int arr2[] = {7, 8, 6, 10, 1};
+    int n=8, m=5;
+    intersection(arr1, arr2, n, m);
+}
