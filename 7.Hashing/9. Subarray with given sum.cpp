2cd641dd527b1c675b0e14e410900defa42816bd
Aman149 <aman001014@gmail.com>
2022-08-22 23:08:54 +0530

    Space O(n), Time O(n).

--- 7.Hashing/9. Subarray with given sum.cpp
+++ 7.Hashing/9. Subarray with given sum.cpp
@@ -1,22 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-bool subarraySum(int arr[], int n) {
+bool subarraySum(int arr[], int n, int sum) {
     unordered_set<int> hash;
     int prefixSum = 0;
 
     for(int i=0; i<n; i++) {
         prefixSum += arr[i];
-        if(hash.find(prefixSum) != hash.end() || prefixSum == 0)
+
+        if(hash.find(prefixSum-sum) != hash.end() || prefixSum == sum)
             return true;
         else
             hash.insert(prefixSum);
-    }  
+    }
     return false;
 }
 
 int main() {
     int arr[] = {-1, 4, -3, 5 ,1};
     int n=5;
-    cout<<subarraySum(arr, n);
+    cout<<subarraySum(arr, n, 3);
 }
